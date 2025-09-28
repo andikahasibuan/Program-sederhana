0a61bce0bf483e7c5a0a879ea9fb963ddd3481fc
andikahasibuan <andikarahmanhsb1@gmail.com>
2025-09-28 19:16:35 +0700

    menambahkan try & except dan juga beberapa perubahan

--- todolist.py
+++ todolist.py
@@ -1,46 +1,47 @@
 tugas = []
 def tambah_tugas(tugas1):
-	tugas2 = input("tambahkan tugas: ")
+	tugas2 = input("Tulis tugas yang ingin di tambahkan: ")
 	tugas1.append(tugas2)
-	lanjut = input("tekan enter untuk lanjut")
-	print("tugas berhasil di tambahkan!")
+	print("Tugas anda berhasil di tambahkan!")
 
 def hapus_tugas(tugas1):
+	print("==Daftar tugas anda==")
 	for nomor, daftar in enumerate(tugas1):
 		print(f"{nomor + 1}. {daftar}")
-	data = int(input("pilih nomor yang ingin dihapus: ")) - 1
+	data = int(input("Silahkan pilih tugas yang ingin di hapus: ")) - 1
 	tugas1.pop(data)
-	print("Tugas berhasil dihapus!")
+	print(f"Tugas nomor {data + 1} berhasil di hapus!")
 		
 
 def lihat_tugas(tugas1):
+	print("==Daftar tugas anda==")
 	for nomor, daftar in enumerate(tugas1):
 		print(f"{nomor + 1}. {daftar}")
-	lanjut = input("tekan enter untuk kembali")
 
 
 def main():
 	print("===to-do list===")
-	print("1. tambah tugas")
-	print("2. hapus tugas")
-	print("3. lihat semua tugas")
-	print("4 keluar")
+	print("1. Tambah tugas")
+	print("2. Lihat semua tugas")
+	print("3. Hapus tugas")
+	print("4. Keluar")
 
 
 while True:
 	main()
-	pilihan = int(input("silahkan pilih nommor yang ada di menu: "))
-	if pilihan > 4:
-		print("nomor tidak valid")
-	elif pilihan == 1:
-		tambah_tugas(tugas)
-	elif pilihan == 2:
-		hapus_tugas(tugas)
-	elif pilihan == 3:
-		lihat_tugas(tugas)
-	elif pilihan ==4:
-		break
-	else:
-		print("kosong")
-
+	try:
+		pilihan = int(input("Silahkan pilih nommor yang ada di menu: "))
+		if pilihan > 4:
+			print("Nomor tidak valid")
+		elif pilihan == 1:
+			tambah_tugas(tugas)
+		elif pilihan == 2:
+			lihat_tugas(tugas)
+		elif pilihan == 3:
+			hapus_tugas(tugas)
+		elif pilihan ==4:
+			print("Program  selesai")
+			break
+	except ValueError:
+		print("Harus memasukkan nomor!")
 
