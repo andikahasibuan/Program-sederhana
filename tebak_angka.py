91d2461fb1f98862d653e8161319084b43866f1f
andikahasibuan <andikarahmanhsb1@gmail.com>
2025-10-01 00:04:15 +0700

    membuat beberapa perubahan pada fungsi tebak_angka dan yang lainnya

--- tebak_angka.py
+++ tebak_angka.py
@@ -1,14 +1,21 @@
 import random
 
 def tebak_angka():
-	angka_random = random.randint(1, 5)
-	pilihan = int(input("Silahkan tebak, angka berapa yang akan muncul dari 1-5:"))
-	if pilihan > 10:
-		print("Angka yang anda masukkan tidak valid!")
-	elif pilihan == angka_random:
-		print("Selamat anda berhasil menebak dengan benar!")
-	elif pilihan != angka_random:
-		print("Anda salah menebak angka!")
+	while True:
+		angka_random = random.randint(1, 5)
+		pilihan = int(input("Silahkan tebak, angka berapa yang akan muncul dari 1-5:"))
+		if pilihan > 5:
+			print("Angka yang anda masukkan tidak valid!")
+		elif pilihan != angka_random:
+			print("Anda salah menebak angka!")
+		else:
+			print("Selamat anda berhasil menebak dengan benar!")
+		pilihan1 = input("ingin lanjut [y/n]?: ")
+		if pilihan1 == "y":
+			return tebak_angka()
+		elif pilihan1 == "n":
+			print("Anda kembali ke menu utama")
+			break			
 
 def keluar():
 	print("Terimah kasih sudah bermain")
@@ -21,13 +28,16 @@ def main():
 
 while True:
 	main()
-	pilihan = int(input("Silahkan pilih: "))
-	if pilihan > 2:
-		print("Angka yang anda masukkan tidak valid!")
-	elif pilihan == 1:
-		tebak_angka()
-	elif pilihan == 2:
-		keluar()
-		break
-	else:
-		print("Pilihan anda tidak valid!")
+	try:
+		pilihan = int(input("Silahkan pilih: "))
+		if pilihan > 2:
+			print("Angka yang anda masukkan tidak valid!")
+		elif pilihan == 1:
+			tebak_angka()
+		elif pilihan == 2:
+			keluar()
+			break
+		else:
+			print("Pilihan anda tidak valid!")
+	except ValueError:
+		print("Input yang anda masukkan tidak valid!")
