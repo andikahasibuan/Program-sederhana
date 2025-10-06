071a9070612bc956c9d7ef349e4fe1026857fb23
andikahasibuan <andikarahmanhsb1@gmail.com>
2025-10-06 14:06:18 +0700

    menambahkan fungsi hapus transaksi dan beberapa perubahan lainnya

--- expense_tracker.py
+++ expense_tracker.py
@@ -19,26 +19,42 @@ def tambah_transaksi(transaksi):
 	kategori = input("Masukkan kategori: ")
 	catatan = input("Tambahkan catatan: ")
 	print("Transaksi anda berhasil ditambahkan!")
-	data = f"({tanggal1}) {jumlah} - {kategori} {catatan}"
-	# data = {
-	# 	"tanggal" : tanggal1,
-	# 	"jumlah" : jumlah,
-	# 	"kategori": kategori,
-	# 	"catatan": catatan,
-	# 	}
+	data = f"({tanggal1}) Rp{jumlah} - {kategori}, {catatan}"
 	transaksi.append(data)
 	with open("data.json", "w") as file:
 		json.dump(transaksi, file, indent = 4, default= str)
 
 		
 
-def lihat_transaksi(transaksi):
-	print("==Riwayat transaksi anda==")
+def lihat_transaksi():
+	while True:
+		print("==Riwayat transaksi anda==")
+		with open("data.json", "r") as file:
+			data = json.load(file)
+		for index, i in enumerate(data):
+			print(f"{index + 1}. {i}")
+		pilihan = input("Ketik q untuk kembali: ")
+		if pilihan != "q":
+			print("Data tidak valid!")
+		else:
+			print("Anda kembali ke menu utama!")
+			break
+
+def hapus_transaksi():
 	with open("data.json", "r") as file:
 		data = json.load(file)
-	for index, i in enumerate(data):
-		print(f"{index + 1}. {i}")
-	
+	for nomor, i in enumerate(data):
+		print(f"{nomor + 1}. {i}")
+	pilihan = int(input("Silahkan pilih data transaksi mana yang ingin di hapus:  ")) -1
+	if pilihan > nomor :
+		print("Data tidak valid!")
+	else:
+		data.pop(pilihan)
+		print("Data transaksi berhasil di hapus!")
+		with open ("data.json", "w") as file:
+			json.dump(data, file)
+			
+
 
 def keluar():
 	print("Anda keluar dari Expense Tracker!")
@@ -47,18 +63,24 @@ def main():
 	print("==Expense Tracker==")
 	print("1. Tambah transaksi")
 	print("2. Lihat transaksi")
-	print("3. Keluar")
+	print("3. Hapus transaksi")
+	print("4. Keluar")
 
 
 while True:
 	main()
-	pilihan = int(input("Silahkan pilih: "))
-	if pilihan > 3:
-		print("Nomor tidak valid")
-	elif pilihan == 1:
-		tambah_transaksi(transaksi)
-	elif pilihan == 2:
-		lihat_transaksi(transaksi)
-	elif pilihan == 3:
-		keluar()
-		break
+	try:
+		pilihan = int(input("Silahkan pilih: "))
+		if pilihan > 4:
+			print("Nomor tidak valid")
+		elif pilihan == 1:
+			tambah_transaksi(transaksi)
+		elif pilihan == 2:
+			lihat_transaksi()
+		elif pilihan == 3:
+			hapus_transaksi()
+		elif pilihan == 4:
+			keluar()
+			break
+	except ValueError:
+		print("Data yang anda masukkan tidak valid!")
